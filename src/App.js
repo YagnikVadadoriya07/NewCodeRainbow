684696bc13846d1c2253f8c5ad20414983d49050
YagnikVadadoriya07 <104341207+YagnikVadadoriya07@users.noreply.github.com>
2022-11-22 17:46:43 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { YourComponent } from "./YourComponent";
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [
+    alchemyProvider({ alchemyId: "m4WoV7TvJZ-emJbnvfJkH4CZAU_nr_8k" }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+});
+
+export default function App() {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <YourComponent />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
